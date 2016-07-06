28d72a6b2fcf6cb229584ca1ae76a65513b04343
Felipe Constantino de Oliveira <felipe.constantino@uux.com>
2016-07-06 16:35:57 -0300

    first commit

---
+++ application/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let express = require('express');
+let app = express();
+
+app.get('/:instanceId/', function (req, res) {
+    let instanceId = req.params['instanceId'];
+    let text = 'Hello! ';
+    text += `The current instance is "${instanceId}""`;
+    console.log(req);
+    res.send(text);
+});
+
+
+let port = process.env.PORT || 3000
+
+// the middleware is initialized now, so start the server 
+app.listen(port);
